3ae328353398ac9e69915f8aa25b69c6f4f78783
gustavo nunez <gn2617@gmail.com>
2016-03-31 21:38:07 -0400

    Initial commit

---
+++ app/src/main/java/com/example/gusta/gigaloan/GigALoanRegistration1.java
@@ -0,0 +1,13 @@
+package com.example.gusta.gigaloan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GigALoanRegistration1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_Registration_User_Type_Page);
+    }
+}
